aa21e923f3800105d31a5c71ad4e939c63481092
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-05-01 11:03:41 +0600

    added font awesome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,34 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    theme: {
+      extend: {
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+          roboto: ["Roboto", "sans-serif"],
+        },
+        colors: {
+          primary: "#fd4d57",
+        },
       },
-    },
+      screen: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"), 
+  ],
 };
 export default config;
